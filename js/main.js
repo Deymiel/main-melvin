42a8841d63f8563c4d18eff8b9c5d2fdea9d35da
braveeggPreviews <maverick.previews@gmail.com>
2023-02-26 14:55:17 +0800

    update for mobile responsive

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 // Cursor
+
+
+
+$(document).ready(function() {
+    $('.toggle-button').on('click', function() {
+      $('.side-element-overlay').toggleClass('active-sidebar');
+      $('.overlay-cover').toggleClass('show');
+    });
+});
